782a6112984d082a953e2d61653542b63b401fc4
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2017-03-13 21:21:21 -0700

    replacing buildout with uranium.

---
+++ ubuild.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+
+def main(build):
+    build.packages.install(".", develop=True)
+
+
+def test(build):
+    main(build)
+    build.packages.install("pytest")
+    build.packages.install("pytest-cov")
+    build.packages.install("pytest-aiohttp")
+    build.packages.install("mock")
+    build.packages.install("httpretty")
+    pytest = os.path.join(build.root, "bin", "py.test")
+    subprocess.call([
+        pytest, "--cov", "yt.formula.node",
+        "yt.formula.node/tests",
+        "--cov-report", "term-missing"
+    ] + build.options.args)
+
+
+def publish(build):
+    """ distribute the uranium package """
+    build.packages.install("wheel")
+    build.executables.run([
+        "python", "setup.py",
+        "sdist", "bdist_wheel", "--universal", "upload"
+    ])
